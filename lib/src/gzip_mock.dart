91b08b2b52fe0170860c638ec5afc7ad99b44002
Moritz <mosum@google.com>
2024-04-30 16:55:12 +0200

    Add docs

--- lib/src/gzip_mock.dart
+++ lib/src/gzip_mock.dart
@@ -1,9 +1,16 @@
 import 'dart:typed_data';
 
+/// The [GZip] encodes raw bytes to GZip compressed bytes and decodes GZip
+/// compressed bytes to raw bytes.
+///
+/// It is implemented using `dart:io` on native platforms and `package:web` in
+/// browsers.
 class GZip {
+  /// Compress the [data] using gzip compression.
   Future<List<int>> compress(Uint8List data) async =>
       throw UnimplementedError();
 
+  /// Decode the gzip-compressed [data].
   Future<List<int>> decompress(Uint8List data) async =>
       throw UnimplementedError();
 }
